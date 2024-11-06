bf6c9cabe7dea7432b989018cf186032c3a52e13
milescb <mcochranbranson@gmail.com>
2024-11-06 12:14:51 -0800

    updates for performance studies

--- submit/run_eval.sh
+++ submit/run_eval.sh
@@ -5,9 +5,10 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 # salloc --nodes 1 --qos interactive --time 04:00:00 --constraint gpu --gpus=1 --account=m3443 --cpus-per-gpu=10
 # shifter --module=gpu --image=docexoty/tritonserver:latest
 
-outdir=${1:-"performance/data/main_traccc"}
-model_name=${2:-"models"}
-multi_gpu=${3:-"false"} # or set to true
+outdir=${1:-"performance/data/send_and_recieve/"}
+model_name=${2:-"models_send_recieve"}
+multi_gpu=${3:-"false"}
+input_data=${4:-"performance/data/perf_data_odd_mu200.json"}
 
 for i in {1..8}
 do
@@ -15,7 +16,7 @@ do
     echo "Running with $i model instances"
     echo "-------------------------------"
     echo ""
-    "$DIR/run_analyzer.sh" $i 1 "" 100000 $ourdir $i $i 1 $model_name
+    "$DIR/run_analyzer.sh" $i 1 "" 100000 $outdir $i $i 1 $model_name $input_data
 done
 
 # plot
@@ -26,8 +27,8 @@ if [ "$multi_gpu" == "true" ]; then
     echo "Running with 1 model instance on 4 GPUs"
     echo "---------------------------------------"
     echo ""
-    "$DIR/run_analyzer.sh" 4 1 "" 100000 $outdir 1 8 1 $model_name
-    "$DIR/run_analyzer.sh" 1 1 "" 100000 $outdir 1 8 1 $model_name
+    "$DIR/run_analyzer.sh" 1 4 "" 100000 $outdir 1 8 1 $model_name $input_data
+    "$DIR/run_analyzer.sh" 1 1 "" 100000 $outdir 1 8 1 $model_name $input_data
 
     python3 "$DIR/../make_multi_gpu_plots.py" --indir=$outdir --outdir=$outdir --n-instances 1
 fi
